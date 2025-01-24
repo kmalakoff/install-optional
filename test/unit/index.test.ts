74151ab5bcc2760fe8a1133adfb3fadc4501c50c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 18:59:14 -0800

    Add cwd option to find, installSync, and removeSync

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import url from 'url';
 import spawn from 'cross-spawn-cb';
+import { install as npmInstall } from 'install-module-linked';
 import mkdirp from 'mkdirp-classic';
 import Pinkie from 'pinkie-promise';
 import rimraf2 from 'rimraf2';
@@ -14,42 +15,45 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 const TMP_DIR = path.join(__dirname, '..', '..', '.tmp');
 const cwd = process.cwd();
 
-function installModule(name, dest) {
+function installModule(name, dest, callback) {
   mkdirp.sync(dest);
   fs.writeFileSync(path.join(dest, 'package.json'), '{}', 'utf8');
-  spawn.sync('npm', ['install', name], { cwd: dest, encoding: 'utf8' });
+  npmInstall(name, dest, callback);
 }
 
 describe('install-optional', () => {
-  beforeEach(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
+  // beforeEach(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
   afterEach(() => process.chdir(cwd));
-  after(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
+  // after(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
 
   describe('install sync', () => {
-    it('installs rollup for this platform', () => {
-      installModule('rollup', TMP_DIR);
-      process.chdir(TMP_DIR);
-      const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
-      removeSync('rollup', `${process.platform}-${process.arch}`);
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
-      installSync('rollup', `${process.platform}-${process.arch}`);
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+    it('installs rollup for this platform', (done) => {
+      installModule('rollup', TMP_DIR, () => {
+        process.chdir(TMP_DIR);
+        const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+        removeSync('rollup', `${process.platform}-`, { cwd: TMP_DIR });
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
+        installSync('rollup', `${process.platform}-`, { cwd: TMP_DIR });
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+        done();
+      });
     });
   });
 
   describe('install callback', () => {
     it('installs rollup for this platform', (done) => {
-      installModule('rollup', TMP_DIR);
-      process.chdir(TMP_DIR);
-      const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
-      removeSync('rollup', `${process.platform}-${process.arch}`);
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
-      install('rollup', `${process.platform}-${process.arch}`, (err) => {
-        if (err) return done(err.message);
+      installModule('rollup', TMP_DIR, () => {
+        process.chdir(TMP_DIR);
+        const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
         assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
-        done();
+        removeSync('rollup', `${process.platform}-`, { cwd: TMP_DIR });
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
+        install('rollup', `${process.platform}-`, { cwd: TMP_DIR }, (err) => {
+          if (err) return done(err.message);
+          assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+          done();
+        });
       });
     });
   });
@@ -69,15 +73,17 @@ describe('install-optional', () => {
       });
     })();
 
-    it('installs rollup for this platform', async () => {
-      installModule('rollup', TMP_DIR);
-      process.chdir(TMP_DIR);
-      const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
-      removeSync('rollup', `${process.platform}-${process.arch}`);
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
-      await install('rollup', `${process.platform}-${process.arch}`);
-      assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+    it('installs rollup for this platform', (done) => {
+      installModule('rollup', TMP_DIR, async () => {
+        process.chdir(TMP_DIR);
+        const modulePath = path.join(TMP_DIR, 'node_modules', 'rollup');
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+        removeSync('rollup', `${process.platform}-`, { cwd: TMP_DIR });
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length === 0);
+        await install('rollup', `${process.platform}-`, { cwd: TMP_DIR });
+        assert.ok(fs.readdirSync(path.join(modulePath, '..', '@rollup')).length > 0);
+        done();
+      });
     });
   });
 });
