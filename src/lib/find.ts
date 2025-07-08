387088dd576d5a4e8ae6ba12fdf07a1179b615ac
Kevin Malakoff <kmalakoff@gmail.com>
2025-07-08 13:16:17 -0700

    Floated dependencies

--- src/lib/find.ts
+++ src/lib/find.ts
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import resolve from 'resolve';
+import * as resolve from 'resolve';
 
 import type { Found, InstallOptions } from '../types.ts';
 
+const resolveSync = (resolve.default ?? resolve).sync;
+
 export default function find(moduleIdentifier: string, match: string, options: InstallOptions = {}): Found[] {
-  const packagePath = resolve.sync(`${moduleIdentifier}/package.json`, options.cwd ? { basedir: options.cwd } : {});
+  const packagePath = resolveSync(`${moduleIdentifier}/package.json`, options.cwd ? { basedir: options.cwd } : {});
   const nodeModules = moduleIdentifier[0] === '@' ? path.join(packagePath, '..', '..', '..') : path.join(packagePath, '..', '..');
   const optionalDependencies = JSON.parse(fs.readFileSync(packagePath, 'utf8')).optionalDependencies || {};
   const results = [];
