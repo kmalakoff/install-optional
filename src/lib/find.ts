46d24d193763c4dc5afa6b2defa192ceda97a39e
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 13:47:09 -0800

    Optional matchedLibc

--- src/lib/find.ts
+++ src/lib/find.ts
@@ -10,9 +10,12 @@ export default function find(moduleIdentifier: string, match: string, options: I
   const packagePath = resolveSync(`${moduleIdentifier}/package.json`, options.cwd ? { basedir: options.cwd } : {});
   const nodeModules = moduleIdentifier[0] === '@' ? path.join(packagePath, '..', '..', '..') : path.join(packagePath, '..', '..');
   const optionalDependencies = JSON.parse(fs.readFileSync(packagePath, 'utf8')).optionalDependencies || {};
+  const filter = options.filter;
   const results = [];
   for (const name in optionalDependencies) {
     if (name.indexOf(match) < 0) continue;
+    // Apply custom filter if provided
+    if (filter && !filter(name)) continue;
     results.push({
       name,
       version: optionalDependencies[name],
