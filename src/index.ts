76e5d3ce1bb2415ab53390f240fbdc1f915ae5ac
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 21:23:58 -0800

    Function overloads

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,16 @@ export type * from './types.ts';
 
 import type { InstallCallback, InstallOptions } from './types.ts';
 import { default as worker } from './workers/install.ts';
-export function install(installString: string, match: string, options?: InstallOptions | InstallCallback, callback?: InstallCallback): undefined | Promise<string> {
+export function install(installString: string, match: string, callback: InstallCallback): void;
+export function install(installString: string, match: string, options: InstallOptions, callback: InstallCallback): void;
+export function install(installString: string, match: string, options?: InstallOptions): Promise<void>;
+export function install(installString: string, match: string, options?: InstallOptions | InstallCallback, callback?: InstallCallback): void | Promise<void> {
   if (typeof options === 'function') {
     callback = options as InstallCallback;
     options = {};
   }
   options = options || {};
 
-  if (typeof callback === 'function') return worker(installString, match, options, callback) as undefined;
-  return new Promise((resolve, reject) => worker(installString, match, options, (err) => (err ? reject(err) : resolve(undefined))));
+  if (typeof callback === 'function') return worker(installString, match, options, callback);
+  return new Promise((resolve, reject) => worker(installString, match, options as InstallOptions, (err) => (err ? reject(err) : resolve())));
 }
