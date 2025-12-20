82c908ef20dbaec34ab4adb254074de39afff7ae
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 12:35:28 -0800

    Migrated to void and simplified compat layer

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,9 @@ export function install(installString: string, match: string, callback: InstallC
 export function install(installString: string, match: string, options: InstallOptions, callback: InstallCallback): void;
 export function install(installString: string, match: string, options?: InstallOptions): Promise<void>;
 export function install(installString: string, match: string, options?: InstallOptions | InstallCallback, callback?: InstallCallback): void | Promise<void> {
-  if (typeof options === 'function') {
-    callback = options as InstallCallback;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as InstallOptions);
 
   if (typeof callback === 'function') return worker(installString, match, options, callback);
-  return new Promise((resolve, reject) => worker(installString, match, options as InstallOptions, (err) => (err ? reject(err) : resolve())));
+  return new Promise((resolve, reject) => worker(installString, match, options, (err) => (err ? reject(err) : resolve())));
 }
